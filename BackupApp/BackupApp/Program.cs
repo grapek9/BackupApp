050dcd87cb3f6edb0e421e39fcc8a9ecaba0bd70
Grapker <zerex93@gmail.com>
2015-09-20 15:25:00 +0200

    Backup Console App (c#)
    added checking files class (target,destination,settings)
    added implementation for checking files class
    todo:
    -create missing files
    -reading from files (target,destination,settings)
    -coping files from target location to destination location

---
+++ BackupApp/BackupApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking for target/destination files");
+            checkFilesClass CheckFiles = new checkFilesClass();
+            CheckFiles.status();
+            Console.ReadKey();
+        }
+    }
+}
