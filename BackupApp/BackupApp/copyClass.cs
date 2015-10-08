7117154835465689e1fa9f2d25a6255d21481e9e
Grapker <zerex93@gmail.com>
2015-10-08 22:56:22 +0200

    changed idea of way of coping, instead of using .net and recursion ill use cmd command xcopy todo add cmd implementation

--- BackupApp/BackupApp/copyClass.cs
+++ BackupApp/BackupApp/copyClass.cs
@@ -12,91 +12,46 @@ namespace BackupApp
         String timeOfBackup = DateTime.Now.ToString().Replace("/", "-").Replace(":", "-").Replace(" ", "_");
         private String[] targets;
         private String[] destinations;
+        private List<String> newDestinations;
         public copyClass(String[] targets, String[] destinations) {
             this.targets = targets;
             this.destinations = destinations;
         }
         public void runCopying() {
+            List<String> executableCommands;
             foreach (String i in destinations) {
-                checkForDestinationFolders(i);
-                copyFiles(i);
+                createDestinationFolders(i);
             }
+            executableCommands = createExecutableCommands(newDestinations);
         }
         private void createDestinationFolders(String destination) {
-            Directory.CreateDirectory(destination);
-            copyFiles(destination);
+            Directory.CreateDirectory(destination+@"\Backup"+timeOfBackup);
+            newDestinations.Add(destination + @"\Backup" + timeOfBackup);
         }
-        private void checkForDestinationFolders(String destination) {
-
-            if (!Directory.Exists(destination + @"Backup" + timeOfBackup)) {
-                createDestinationFolders(destination + @"Backup" + timeOfBackup);
+        
+        private void copyFiles(List<String> commands) {
+            foreach (String command in commands) {
+                executeCommand(command);
             }
-        }
-        private void copyFiles(String destination) {
-            String nameOfFolder = "";
-            String destDirFolder = "";
-            foreach (String i in targets) {
-                nameOfFolder = extractName(i);
-                if (!Directory.Exists(destination + @"\Backup" + timeOfBackup+"\\"+nameOfFolder))
-                {
-                    createDestinationFolders(destination + @"\Backup" + timeOfBackup + "\\" + nameOfFolder);
-                }
-                destDirFolder = destination + @"\Backup" + timeOfBackup + "\\" + nameOfFolder;
-                DirectoryInfo dir = new DirectoryInfo(i);
-                DirectoryInfo[] directories = dir.GetDirectories();       
-                FileInfo[] files = dir.GetFiles();
-                foreach (FileInfo file in files)
-                {
-                    string temppath = Path.Combine(destDirFolder, file.Name);
-                    file.CopyTo(temppath, false);
-                }
-                foreach (DirectoryInfo subdir in directories)
-                {
-                    string temppath = Path.Combine(destDirFolder, subdir.Name);
-                    File.Copy(subdir.FullName, temppath, true);
-                }
-            }
+            
         }
         private String extractName(String path) {
             String[] name = path.Split('/');
             return name[name.Length-1];
         }
-        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
-        {
-            // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
-
-            if (!dir.Exists)
-            {
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + sourceDirName);
-            }
-
-            DirectoryInfo[] dirs = dir.GetDirectories();
-            // If the destination directory doesn't exist, create it.
-            if (!Directory.Exists(destDirName))
-            {
-                Directory.CreateDirectory(destDirName);
-            }
-
-            // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files)
-            {
-                string temppath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(temppath, false);
-            }
-
-            // If copying subdirectories, copy them and their contents to new location.
-            if (copySubDirs)
-            {
-                foreach (DirectoryInfo subdir in dirs)
-                {
-                    string temppath = Path.Combine(destDirName, subdir.Name);
-                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+        private List<String> createExecutableCommands(List<String> destinations) {
+            List<String> commands = null;
+            foreach (String dest in destinations) {
+                foreach (String target in targets) {
+                    commands.Add("/C "+target+" "+dest+" /S /I");
                 }
             }
+            return commands;
         }
+        private void executeCommand(String command)
+        {
+            
+        }
+        
     }
 }
