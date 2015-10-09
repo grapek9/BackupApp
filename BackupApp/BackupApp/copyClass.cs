6ee9a4f1732c12e52ddbc9ef7910e534bc900771
Grapker <zerex93@gmail.com>
2015-10-09 22:57:46 +0200

    added implementation for cmd command todo: test funcionality

--- BackupApp/BackupApp/copyClass.cs
+++ BackupApp/BackupApp/copyClass.cs
@@ -50,7 +50,14 @@ namespace BackupApp
         }
         private void executeCommand(String command)
         {
-            
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
         }
         
     }
