90153e51c80c2a169a76bc6b94a742b32bdaaa84
Grapker <zerex93@gmail.com>
2015-10-10 16:01:31 +0200

    done <not important> todo cosmetic changes

--- BackupApp/BackupApp/copyClass.cs
+++ BackupApp/BackupApp/copyClass.cs
@@ -12,7 +12,7 @@ namespace BackupApp
         String timeOfBackup = DateTime.Now.ToString().Replace("/", "-").Replace(":", "-").Replace(" ", "_");
         private String[] targets;
         private String[] destinations;
-        private List<String> newDestinations;
+        private List<String> newDestinations = new List<String>();
         public copyClass(String[] targets, String[] destinations) {
             this.targets = targets;
             this.destinations = destinations;
@@ -22,42 +22,47 @@ namespace BackupApp
             foreach (String i in destinations) {
                 createDestinationFolders(i);
             }
+            Console.WriteLine("test");
             executableCommands = createExecutableCommands(newDestinations);
+            copyFiles(executableCommands);
+            
         }
         private void createDestinationFolders(String destination) {
-            Directory.CreateDirectory(destination+@"\Backup"+timeOfBackup);
-            newDestinations.Add(destination + @"\Backup" + timeOfBackup);
+            Directory.CreateDirectory(destination+@"Backup"+timeOfBackup);
+            Console.WriteLine(destination + @"Backup" + timeOfBackup);
+            newDestinations.Add(destination + @"Backup" + timeOfBackup);
+            
         }
         
-        private void copyFiles(List<String> commands) {
-            foreach (String command in commands) {
+        private void copyFiles(List<String> commands)
+        {
+            foreach (String command in commands)
+            {
                 executeCommand(command);
             }
             
         }
-        private String extractName(String path) {
-            String[] name = path.Split('/');
-            return name[name.Length-1];
-        }
         private List<String> createExecutableCommands(List<String> destinations) {
-            List<String> commands = null;
+            List<String> commands = new List<string>();
             foreach (String dest in destinations) {
                 foreach (String target in targets) {
-                    commands.Add("/C "+target+" "+dest+" /S /I");
+                    commands.Add("/C xcopy \""+target+"\" \""+dest+"\" /S /I");
                 }
             }
             return commands;
         }
         private void executeCommand(String command)
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            Console.WriteLine("Executing command : "+ command);
+            /*System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
             startInfo.Arguments = command;
             process.StartInfo = startInfo;
-            process.Start();
-            process.Close();
+            process.Start();*/
+            System.Diagnostics.Process.Start("CMD.exe", command);
+
         }
         
     }
